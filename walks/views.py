45b5f3b64c05c722e01d0c6d3deece9f4781c3a3
Lucy Norman <lucynorman@mac.com>
2024-11-25 09:48:02 +0000

    Testing set up works with html template

--- walks/views.py
+++ walks/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 # Create your views here.
 
 def test_message(request):
     return HttpResponse("Hello walker!")
+
+class HomePage(TemplateView):
+    """
+    Displays home page
+    """
+    template_name = 'index.html'    
