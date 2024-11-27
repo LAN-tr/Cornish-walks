7b74b71a6568ef0108f690fdec1aeb120927c94d
Lucy Norman <lucynorman@mac.com>
2024-11-27 14:35:19 +0000

    set up first fields in walk model

--- walks/admin.py
+++ walks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Walk
 
 # Register your models here.
+
+admin.site.register(Walk)
--- walks/models.py
+++ walks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Walk(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
